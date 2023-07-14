d45a0b0be5d1d3aeba132cc308bf2c582f4894b7
Adrienne Galloway <adrienneg@ext.inditex.com>
2023-07-14 10:36:54 +0100

    Add sample-directory with one tf file

---
+++ sample-directory/main.tf
@@ -0,0 +1,28 @@
+resource "azurerm_resource_group" "example" {
+  name     = "database-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_mssql_server" "example" {
+  name                         = "mssqlserver"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "missadministrator"
+  administrator_login_password = "thisIsKat11"
+  
+  # MIN TLS Version has to be 1.2
+  # minimum_tls_version          = "1.2"
+
+  # No Public Access to SQL Server 
+  public_network_access_enabled = "false"
+
+  azuread_administrator {
+    login_username = "AzureAD Admin"
+    object_id      = "00000000-0000-0000-0000-000000000000"
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
